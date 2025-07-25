adc3b831daa880bd1ba1ea743a44763b7910eda9
bcheban <128683111+bcheban@users.noreply.github.com>
2025-07-25 20:24:11 +0300

    added changes

--- components/header.tsx
+++ components/header.tsx
@@ -93,7 +93,7 @@ export const HeroHeader = () => {
               <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
                 <div className="flex items-center">
                   <Link
-                    href="https://github.com/TheOrcDev/noteforge"
+                    href="https://github.com/bcheban/next-noteforge"
                     target="_blank"
                   >
                     <Button
